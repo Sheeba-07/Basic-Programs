98a96ebee13bad7717674ece949286c6092e99c8
Sheeba Mariyam <mmariyamsheeba@gmail.com>
2024-11-17 16:44:47 +0530

    logical.java

---
+++ logical.java
@@ -0,0 +1,12 @@
+package logical;
+public class logical {
+    public static void main(String[]args){
+   int a,b,c;
+    a=40;
+    b=56;
+    c=70;
+    System.out.println(a<c&&b>a);
+    System.out.println(a<=c&&b<=a);
+    System.out.println(a<=c&&b<a);
+    }
+   }
